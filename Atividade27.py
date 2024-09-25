b37d0afd8588def33c50b9c5c46fe9d9ba83d0b2
Miguel-nextage <Pmiguelhkss@gmail.com>
2024-09-25 15:06:32 -0300

    Convidados

---
+++ Atividade27.py
@@ -0,0 +1,33 @@
+# Solicite ao usuário que insira o nome de até 7 convidados.
+# Armazene os nomes em uma lista.
+# Permita ao usuário remover um convidado da lista, caso necessário.
+# Exiba a lista final de convidados.
+
+# Digite o nome do convidado 1: João
+# Digite o nome do convidado 2: Maria
+# ...
+# Digite o nome do convidado 7: Pedro
+# Deseja remover algum convidado da lista? (sim/não): sim
+# Digite o nome do convidado a ser removido: Maria
+
+lista = []
+sans = str("o")
+
+for c in range(7):
+    convidado = input(F"Digite o nome do {c + 1} convidado")
+
+    lista.append(convidado)
+
+    if c == 7:
+        sans = input("Deseja remover algum convidado? (S/N)")
+
+if sans == "sim":
+        Rmv = input("Digite o nome do convidado:")
+        lista.remove(Rmv)
+        print(lista)
+else:
+    print("Pois tá")
+
+   
+
+    
